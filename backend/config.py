05914e2b84de673ca0656b013a9fc99ca1f8a9e8
mavericksoebroto <mavericksoebroto@gmail.com>
2025-12-12 02:27:26 +0800

    Database Systems Final Project

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,5 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "devkey")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://root:maverick2005@localhost/inventory_db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
